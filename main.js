2221d2936622218e3ba7f4a84a63020f9cd36b34
Axel Garcia <axel.garcia94@gmail.com>
2018-10-05 17:05:16 +0200

    adding button disable

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function main () {
-  console.log('JS Running');
+  const button = document.querySelector('.button');
+  button.addEventListener('click', () => {
+    button.disabled = true;
+    button.className = 'button-reverse';
+  })
 }
 
 window.addEventListener('load', main);
